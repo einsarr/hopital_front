cbc0be2c577786a60738161f1c8565415ef1b3cc
Moussa SARR <einsarr@gmail.com>
2023-09-27 19:45:43 +0000

    guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,29 +20,30 @@ import { UsersComponent } from './components/users/users/users.component';
 import { NewUserComponent } from './components/users/new-user/new-user.component';
 import { NewServiceComponent } from './components/services/new-service/new-service.component';
 import { EditUserComponent } from './components/users/edit-user/edit-user.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {path:'',component:LoginComponent},
   {path:'login',component:LoginComponent},
-  {path:'menu',component:MenuComponent},
-  {path:'dashboard',component:DashboardComponent},
-  {path:'admintemplate',component:AdminTemplateComponent},
-  {path:'signup',component:SignupComponent},
-  {path:'lits',component:LitsComponent},
-  {path:'newlit',component:NewLitComponent},
-  {path:'editlit/:id',component:EditLitComponent},
-  {path:'services',component:ServicesComponent},
-  {path:'newservice',component:NewServiceComponent},
-  {path:'editservice/:id',component:EditServiceComponent},
-  {path:'salles',component:SallesComponent},
-  {path:'newsalle',component:NewSalleComponent},
-  {path:'editsalle/:id',component:EditSalleComponent},
-  {path:'welcome',component:WelcomeComponent},
+  {path:'menu',component:MenuComponent, canActivate:[AuthGuard]},
+  {path:'dashboard',component:DashboardComponent, canActivate:[AuthGuard]},
+  {path:'admintemplate',component:AdminTemplateComponent, canActivate:[AuthGuard]},
+  {path:'signup',component:SignupComponent, canActivate:[AuthGuard]},
+  {path:'lits',component:LitsComponent, canActivate:[AuthGuard]},
+  {path:'newlit',component:NewLitComponent, canActivate:[AuthGuard]},
+  {path:'editlit/:id',component:EditLitComponent, canActivate:[AuthGuard]},
+  {path:'services',component:ServicesComponent, canActivate:[AuthGuard]},
+  {path:'newservice',component:NewServiceComponent, canActivate:[AuthGuard]},
+  {path:'editservice/:id',component:EditServiceComponent, canActivate:[AuthGuard]},
+  {path:'salles',component:SallesComponent, canActivate:[AuthGuard]},
+  {path:'newsalle',component:NewSalleComponent, canActivate:[AuthGuard]},
+  {path:'editsalle/:id',component:EditSalleComponent, canActivate:[AuthGuard]},
+  {path:'welcome',component:WelcomeComponent, canActivate:[AuthGuard]},
   {path:'forgetpwd',component:ForgetpwdComponent},
-  {path:'contacts',component:ContactsComponent},
-  {path:'users',component:UsersComponent},
-  {path:'newuser',component:NewUserComponent},
-  {path:'edituser/:id',component:EditUserComponent},
+  {path:'contacts',component:ContactsComponent, canActivate:[AuthGuard]},
+  {path:'users',component:UsersComponent, canActivate:[AuthGuard]},
+  {path:'newuser',component:NewUserComponent, canActivate:[AuthGuard]},
+  {path:'edituser/:id',component:EditUserComponent, canActivate:[AuthGuard]},
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'clinique';
+export class AppComponent implements DoCheck{
+  isMenuRequire = false;
+  title = 'angularapp';
+  constructor(private router:Router){}
+    ngDoCheck():void{
+      let currentUrl = this.router.url;
+      if(currentUrl == '/login' || currentUrl == '/signup'|| currentUrl == '/forgetpwd'|| currentUrl == '/'){
+        this.isMenuRequire = false;
+      }else{
+        this.isMenuRequire = true;
+      }
+    }
 }
